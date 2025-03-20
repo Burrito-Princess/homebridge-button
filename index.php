b8348e742d7b4db9e5f8d322519c6cfaad3c2acf
Burrito-Princess <113419432+Burrito-Princess@users.noreply.github.com>
2025-03-20 23:49:15 +0100

    css start

--- index.php
+++ index.php
@@ -3,15 +3,29 @@
 ?>
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<link rel="stylesheet" href="./assets/css/style.css">
 </head>
-<button onclick="foo()">Click me!</button>
+
+<div id="container">
+  <button id="button" onclick="foo()">Click me!</button>
+</div>
+
 <script>
      function foo () {
       $.ajax({
         url:"toggleButton.php",
         type: "POST",
      });
+     document.getElementById("button").style.backgroundColor = "green";
+     
+
+// stop for sometime if needed
+setTimeout(myFunction, 1000);
  }
+ 
+ function myFunction() {
+  document.getElementById("button").style.backgroundColor = "white";
+}
 </script>
 <?php
 
